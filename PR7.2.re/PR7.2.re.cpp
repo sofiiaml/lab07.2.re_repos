480c91c2038e647709b8c51c89b632a7f534f182
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-11-08 20:52:16 +0200

    pr7.2.re

---
+++ PR7.2.re/PR7.2.re.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std; 
+void Create(int** a, const int n, const int k, const int Low,
+	const int High, int nIndex) {
+	if (nIndex == n)
+		return;
+	for (int i = 0; i < k; i++) {
+		a[nIndex][i] = Low + rand() % (High - Low + 1);
+	}
+	Create(a,n, k, Low, High, nIndex + 1);
+}
+void Print(int** a, const int n, const int k, int nIndex) {
+	if (nIndex == n)
+		return;
+	for (int j = 0; j < k; j++)
+		cout << setw(4) << a[nIndex][j];
+	cout << endl;
+	Print(a, n, k, nIndex + 1);
+}
+
+void Sort(int** a, const int n, const int k, int i) {
+	if (i < n - 1) {
+		int imin = 0;
+		int imax = 0;
+		for (int j = 0; j < k; j++)
+		{
+			if (a[i][j] < a[i][imin])
+				imin = j;
+			if (a[i + 1][j] > a[i + 1][imax])
+				imax = j;
+
+		}
+		int temp = a[i][imin];
+		a[i][imin] = a[i + 1][imax];
+		a[i + 1][imax] = temp;
+		Sort(a, n, k, i + 2);
+	}
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	int Low = 7;
+	int High = 65;
+	int n, k;
+	cout << "n = "; cin >> n;
+	cout << "k = "; cin >> k;
+	int** a = new int* [n];
+	for (int i = 0; i < n; i++)
+		a[i] = new int[k];
+	Create(a, n, k, Low, High,0);
+	cout << "Array:"; cout << endl;
+	Print(a, n, k,0);
+	Sort(a, n, k,0);
+	cout << "Changed Array:"; cout << endl;
+	Print(a, n, k,0);
+	delete[] a;
+	return 0;
+}
---
+++ UnitTest7.2.re/UnitTest7.2.re.cpp
@@ -0,0 +1,55 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR7.2.re/PR7.2.re.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest72re
+{
+	TEST_CLASS(UnitTest72re)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int** arr1 = new int* [3];
+			for (int i = 0; i < 3; i++) {
+				arr1[i] = new int[3];
+			}
+			arr1[0][0] = 3;
+			arr1[0][1] = 5;
+			arr1[0][2] = 7;
+			arr1[1][0] = 9;
+			arr1[1][1] = 13;
+			arr1[1][2] = 1;
+			arr1[2][0] = 34;
+			arr1[2][1] = 8;
+			arr1[2][2] = 21;
+			Sort(arr1, 3, 3,0);
+			int** arr2 = new int* [3];
+			for (int i = 0; i < 3; i++) {
+				arr2[i] = new int[3];
+			}
+			arr2[0][0] = 13;
+			arr2[0][1] = 5;
+			arr2[0][2] = 7;
+			arr2[1][0] = 9;
+			arr2[1][1] = 3;
+			arr2[1][2] = 1 ;
+			arr2[2][0] = 34;
+			arr2[2][1] = 8;
+			arr2[2][2] = 21;
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					Assert::AreEqual(arr2[i][j], arr1[i][j]);
+				}
+			}
+
+			for (int i = 0; i < 3; i++) {
+				delete[] arr1[i];
+				delete[] arr2[i];
+			}
+			delete[]arr1, arr2;
+		}
+	};
+}
